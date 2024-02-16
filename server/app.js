9a522dcceeba512777c5513e905f9755c136c9e2
Luis <luis.ortiz.rivera@alumnos.upm.es>
2024-02-16 16:35:48 +0200

    installed basic packages and config files

--- server/app.js
+++ server/app.js
@@ -1,20 +1,56 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const path = require("path");
+const booksRouter = require("./routes/book");
+const mongoose = require("mongoose");
+const { url } = require("../config/databaseConfig");
+const cors = require("cors");
+const { constants } = require("buffer");
+require("dotenv").config();
 
-var app = express();
+// #region Database connection
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
 
-app.use(logger('dev'));
+// #endregion
+
+const app = express();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use("/api/book", booksRouter);
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://localhost:1234",
+    changeOrigin: true,
+  })
+);
+
+if (process.env.NODE_ENV.trim() === "production") {
+  app.use(express.static(path.resolve("..", "client", "build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("..", "client", "build", "index.html"))
+  );
+} else if (process.env.NODE_ENV.trim() === "development") {
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+}
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running on port ${app.get("port")}`);
+});
 
 module.exports = app;
