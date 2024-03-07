f554980b7fcfa0d0a59869fb87be6690887938fe
Luis <luis.ortiz.rivera@alumnos.upm.es>
2024-03-07 19:02:05 +0200

    Added pagination of matches

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import UserMatchCard from "./Match";
+import Pagination from "react-bootstrap/Pagination";
 const uuid = require("uuid");
 
 const Matches = (props) => {
   const [usersWithMatches, setUsersWithMatches] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 5;
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = usersWithMatches.slice(
+    indexOfFirstUser,
+    indexOfLastUser
+  );
+  let active = currentPage;
+  let items = Array.from(
+    { length: Math.ceil(usersWithMatches.length / usersPerPage) },
+    (_, number) => (
+      <Pagination.Item
+        key={number + 1}
+        active={number + 1 === active}
+        onClick={() => setCurrentPage(number + 1)}
+      >
+        {number + 1}
+      </Pagination.Item>
+    )
+  );
+
   const handleOpenChat = async (userMatchedData) => {
     try {
       const match = props.matches.find((match) => {
@@ -16,7 +39,12 @@ const Matches = (props) => {
         );
       });
 
-      if (match) props.setShowChatBox({ name: userMatchedData.name, surname: userMatchedData.surname, matchId: match._id });
+      if (match)
+        props.setShowChatBox({
+          name: userMatchedData.name,
+          surname: userMatchedData.surname,
+          matchId: match._id,
+        });
     } catch (error) {
       console.error("Error:", error);
     }
@@ -52,14 +80,20 @@ const Matches = (props) => {
         height: "100%",
         float: "right",
         display: "flex",
+        flexDirection: "column",
       }}
     >
-      <Accordion defaultActiveKey="0" style={{ width: "100%" }}>
+      <Accordion
+        defaultActiveKey="0"
+        style={{ width: "100%", marginBottom: "3px" }}
+      >
         <Accordion.Item eventKey="0">
           <Accordion.Header> Your Matches</Accordion.Header>
-          <Accordion.Body style={{ padding: 0, textAlign: "center" }}>
-            {usersWithMatches.length > 0
-              ? usersWithMatches.map((user) => (
+          <Accordion.Body
+            style={{ padding: 0, textAlign: "center", width: "100%" }}
+          >
+            {currentUsers.length > 0
+              ? currentUsers.map((user) => (
                   <UserMatchCard
                     key={uuid.v4()}
                     _id={user.userMatchedData._id}
@@ -73,6 +107,7 @@ const Matches = (props) => {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
+      <Pagination>{items}</Pagination>
     </div>
   );
 };
